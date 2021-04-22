40d03faddbd4f7e8553484b9a3acfbe562a70fd7
Dmitry_Abramov <mf-90@@mail.ru>
2021-04-22 22:43:37 +0400

    Task_8.Routing: add link to back home

--- src/pages/not-found/components/main/Main.tsx
+++ src/pages/not-found/components/main/Main.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Main, Headline, Button } from '@components';
 import cn from "classnames/bind";
 import styles from '@styles/components/index.scss';
@@ -46,9 +47,11 @@ export const NotFoundMain: React.FC<{}> = () => {
                 <span>404</span>
             </div>
             <div className={btnWrapperCn}>
-                <Button classNames={btnCn}>
-                    <span className={btnContentCn}>{'GO BACK TO HOME'}</span>
-                </Button>
+                <Link to="/">
+                    <Button classNames={btnCn}>
+                        <span className={btnContentCn}>{'GO BACK TO HOME'}</span>
+                    </Button>
+                </Link>
             </div>
         </Main>
     )
